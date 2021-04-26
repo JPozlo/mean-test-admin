c8639f59b7e11c5bfebb64365bf17dbe92723625
JPozlo <ozlocollins@gmail.com>
2021-04-26 14:00:02 +0300

    Configured RabbitMQ

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,12 +1,21 @@
-import { Body, Delete, Put } from '@nestjs/common';
+import { Body, Delete, Inject, Put } from '@nestjs/common';
 import { Controller, Get, Param, Post } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 import { ProductService } from './product.service';
 
 @Controller('products')
 export class ProductController {
 
-    constructor(private productService: ProductService){
+    constructor(private productService: ProductService,
+        @Inject("PRODUCT_SERVICE") private readonly client: ClientProxy){
 
+    }
+
+    @Post("/:id/like")
+    async addLike(@Param("id") id: number){
+        const product = await this.productService.getProduct(id);
+
+        return await this.productService.update(id, {likes: product.likes + 1})
     }
 
     @Get("")
@@ -22,18 +31,30 @@ export class ProductController {
     @Post()
     async  create(@Body() body){
        const  { name: name, image: image} = body;
-       return this.productService.create({name, image});
+
+       const product = await this.productService.create({name, image});
+       this.client.emit("product_create", product);
+
+       return product;
     }
 
     @Put("/:id")
     async update(@Param("id") id: number, @Body() body)
     {
-        return this.productService.update(id, body);
+        await this.productService.update(id, body);
+
+        const product = await this.productService.getProduct(id);
+        this.client.emit("product_update", product);
+
+        return product;
     }
 
     @Delete("/:id")
     async delete(@Param("id") id: number){
-        return this.productService.delete(id);
+        await this.productService.delete(id);
+
+        this.client.emit("product_delete", id);
     }
+
 
 }
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductController } from './product.controller';
 import { Product } from './product.entity';
@@ -6,7 +7,20 @@ import { ProductService } from './product.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Product])
+    TypeOrmModule.forFeature([Product]),
+    ClientsModule.register([
+      {
+        name: 'PRODUCT_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqps://efmwgwet:uUK3DISxW4t65yjJAaU2uFqJPD9eIENB@clam.rmq.cloudamqp.com/efmwgwet'],
+          queue: 'user_queue',
+          queueOptions: {
+            durable: false
+          },
+        },
+      },
+    ]),
   ],
   controllers: [ProductController],
   providers: [ProductService]
